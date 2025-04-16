20ec4d9b58db279147db64f6883eee5222d22137
Liu Hongtian <liuhongtian@outlook.com>
2025-04-16 11:10:09 +0800

    添加Prometheus样例页面

---
+++ src/views/kanban/appmon/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app-container">
+      <iframe src="http://www.liuhongtian.com:3000/public-dashboards/e071af50070b422695c24bc321d212cd" height="100%" width="100%"></iframe>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+    
+  <style lang="scss" scoped>
+    .app-container {
+      padding: 20px;
+      height: calc(100vh - 84px);
+      //sbackground-color: rgb(255, 255, 255);
+    }
+  </style> 
